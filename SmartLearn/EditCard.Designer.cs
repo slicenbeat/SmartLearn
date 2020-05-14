271df081a325fc29b629774ca743a06daac18d33
hitaboom <hitaboom@gmail.com>
2020-05-15 00:21:18 +0400

    Фикс окно

--- SmartLearn/EditCard.Designer.cs
+++ SmartLearn/EditCard.Designer.cs
@@ -39,10 +39,9 @@
             // 
             this.lAnswer.AutoSize = true;
             this.lAnswer.FontWeight = MetroFramework.MetroLabelWeight.Regular;
-            this.lAnswer.Location = new System.Drawing.Point(47, 149);
-            this.lAnswer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lAnswer.Location = new System.Drawing.Point(35, 121);
             this.lAnswer.Name = "lAnswer";
-            this.lAnswer.Size = new System.Drawing.Size(48, 20);
+            this.lAnswer.Size = new System.Drawing.Size(46, 19);
             this.lAnswer.TabIndex = 9;
             this.lAnswer.Text = "Ответ";
             // 
@@ -50,19 +49,17 @@
             // 
             this.lQuestion.AutoSize = true;
             this.lQuestion.FontWeight = MetroFramework.MetroLabelWeight.Regular;
-            this.lQuestion.Location = new System.Drawing.Point(47, 100);
-            this.lQuestion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lQuestion.Location = new System.Drawing.Point(35, 81);
             this.lQuestion.Name = "lQuestion";
-            this.lQuestion.Size = new System.Drawing.Size(61, 20);
+            this.lQuestion.Size = new System.Drawing.Size(55, 19);
             this.lQuestion.TabIndex = 8;
             this.lQuestion.Text = "Вопрос";
             // 
             // bEditCard
             // 
-            this.bEditCard.Location = new System.Drawing.Point(557, 198);
-            this.bEditCard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bEditCard.Location = new System.Drawing.Point(418, 161);
             this.bEditCard.Name = "bEditCard";
-            this.bEditCard.Size = new System.Drawing.Size(151, 28);
+            this.bEditCard.Size = new System.Drawing.Size(113, 23);
             this.bEditCard.TabIndex = 7;
             this.bEditCard.Text = "Сохранить";
             this.bEditCard.UseSelectable = true;
@@ -74,18 +71,16 @@
             // 
             // 
             this.tAnswer.CustomButton.Image = null;
-            this.tAnswer.CustomButton.Location = new System.Drawing.Point(676, 2);
-            this.tAnswer.CustomButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tAnswer.CustomButton.Location = new System.Drawing.Point(284, 1);
             this.tAnswer.CustomButton.Name = "";
-            this.tAnswer.CustomButton.Size = new System.Drawing.Size(31, 28);
+            this.tAnswer.CustomButton.Size = new System.Drawing.Size(16, 17);
             this.tAnswer.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.tAnswer.CustomButton.TabIndex = 1;
             this.tAnswer.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.tAnswer.CustomButton.UseSelectable = true;
             this.tAnswer.CustomButton.Visible = false;
             this.tAnswer.Lines = new string[0];
-            this.tAnswer.Location = new System.Drawing.Point(175, 149);
-            this.tAnswer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tAnswer.Location = new System.Drawing.Point(131, 121);
             this.tAnswer.MaxLength = 32767;
             this.tAnswer.Name = "tAnswer";
             this.tAnswer.PasswordChar = '\0';
@@ -94,7 +89,7 @@
             this.tAnswer.SelectionLength = 0;
             this.tAnswer.SelectionStart = 0;
             this.tAnswer.ShortcutsEnabled = true;
-            this.tAnswer.Size = new System.Drawing.Size(533, 28);
+            this.tAnswer.Size = new System.Drawing.Size(400, 23);
             this.tAnswer.TabIndex = 6;
             this.tAnswer.UseSelectable = true;
             this.tAnswer.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -107,18 +102,16 @@
             // 
             // 
             this.tQuestion.CustomButton.Image = null;
-            this.tQuestion.CustomButton.Location = new System.Drawing.Point(676, 2);
-            this.tQuestion.CustomButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tQuestion.CustomButton.Location = new System.Drawing.Point(284, 1);
             this.tQuestion.CustomButton.Name = "";
-            this.tQuestion.CustomButton.Size = new System.Drawing.Size(31, 28);
+            this.tQuestion.CustomButton.Size = new System.Drawing.Size(16, 17);
             this.tQuestion.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.tQuestion.CustomButton.TabIndex = 1;
             this.tQuestion.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.tQuestion.CustomButton.UseSelectable = true;
             this.tQuestion.CustomButton.Visible = false;
             this.tQuestion.Lines = new string[0];
-            this.tQuestion.Location = new System.Drawing.Point(175, 100);
-            this.tQuestion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tQuestion.Location = new System.Drawing.Point(131, 81);
             this.tQuestion.MaxLength = 32767;
             this.tQuestion.Name = "tQuestion";
             this.tQuestion.PasswordChar = '\0';
@@ -127,7 +120,7 @@
             this.tQuestion.SelectionLength = 0;
             this.tQuestion.SelectionStart = 0;
             this.tQuestion.ShortcutsEnabled = true;
-            this.tQuestion.Size = new System.Drawing.Size(533, 28);
+            this.tQuestion.Size = new System.Drawing.Size(400, 23);
             this.tQuestion.TabIndex = 5;
             this.tQuestion.UseSelectable = true;
             this.tQuestion.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -136,19 +129,18 @@
             // 
             // EditCard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(761, 249);
+            this.ClientSize = new System.Drawing.Size(571, 202);
             this.Controls.Add(this.lAnswer);
             this.Controls.Add(this.lQuestion);
             this.Controls.Add(this.bEditCard);
             this.Controls.Add(this.tAnswer);
             this.Controls.Add(this.tQuestion);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.MaximumSize = new System.Drawing.Size(761, 249);
-            this.MinimumSize = new System.Drawing.Size(761, 249);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(571, 202);
+            this.MinimumSize = new System.Drawing.Size(571, 202);
             this.Name = "EditCard";
-            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
             this.Text = "Редактировать карту";
             this.Load += new System.EventHandler(this.EditCard_Load);
             this.ResumeLayout(false);
