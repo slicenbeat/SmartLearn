e9d3b742d7381e3c31259a39f562673d467780f5
hitaboom <hitaboom@gmail.com>
2020-05-14 23:07:54 +0400

    ))))))

--- SmartLearn/EditCard.cs
+++ SmartLearn/EditCard.cs
@@ -36,17 +36,31 @@ namespace SmartLearn
             string name = this.Deck.GetName();
             int i = index++;
             SQLiteCommand CMD = DB.CreateCommand();
-            CMD.CommandText = @"UPDATE '" + name + "' SET question = @question WHERE id = @" + i.ToString();
-            //CMD.CommandText = "INSERT INTO'" + name + "'(question, answer) VALUES( @question , @answer ); ";
+            CMD.CommandText = @"UPDATE " + name +" SET question= @question WHERE id= @" + i.ToString();
             CMD.Connection = new SQLiteConnection();
             CMD.Parameters.Add(new SQLiteParameter("@id", i));
             CMD.Parameters.Add(new SQLiteParameter("@question", tQuestion.Text));
             CMD.ExecuteNonQuery();
-            //Card c = new Card(tQuestion.Text, tAnswer.Text);
             this.Deck.GetList(index).SetQuestion(tQuestion.Text);
             this.Deck.GetList(index).SetAnswer(tAnswer.Text);
             tQuestion.Clear();
             tAnswer.Clear();
+
+
+
+
+            /*string baseName = "Printers.db3";
+            using (var connection = new SQLiteConnection())
+            {
+                connection.ConnectionString = "Data Source = " + baseName;
+                connection.Open();
+                cmd.CommandText = @"UPDATE Kollvo SET Kollvo_Cart= @Kollvo_Cart WHERE id_cart= @ID_Cart";
+                cmd.Connection = connection;
+                cmd.Parameters.Add(new SQLiteParameter("@ID_Cart", listBox1.SelectedValue));
+                cmd.Parameters.Add(new SQLiteParameter("@Kollvo_Cart", numericUpDown1.Text));
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }*/
         }
 
         private void tQuestion_Click(object sender, EventArgs e)
