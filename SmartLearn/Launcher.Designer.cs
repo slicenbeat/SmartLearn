c7f8295f0f4f975858b075b2b5778be7e8df0f62
hitaboom <hitaboom@gmail.com>
2020-05-19 09:44:40 +0400

    Деактивировали кнопку

--- SmartLearn/Launcher.Designer.cs
+++ SmartLearn/Launcher.Designer.cs
@@ -49,10 +49,9 @@ namespace SmartLearn
             // 
             this.CheckTheme.AutoSize = true;
             this.CheckTheme.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.CheckTheme.Location = new System.Drawing.Point(425, 487);
-            this.CheckTheme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CheckTheme.Location = new System.Drawing.Point(319, 396);
             this.CheckTheme.Name = "CheckTheme";
-            this.CheckTheme.Size = new System.Drawing.Size(100, 17);
+            this.CheckTheme.Size = new System.Drawing.Size(93, 15);
             this.CheckTheme.TabIndex = 7;
             this.CheckTheme.Text = "Тёмная тема";
             this.CheckTheme.UseSelectable = true;
@@ -62,25 +61,26 @@ namespace SmartLearn
             // 
             this.LearningButton.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.LearningButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LearningButton.Enabled = false;
             this.LearningButton.FontSize = MetroFramework.MetroButtonSize.Medium;
             this.LearningButton.FontWeight = MetroFramework.MetroButtonWeight.Regular;
-            this.LearningButton.Location = new System.Drawing.Point(203, 202);
-            this.LearningButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LearningButton.Location = new System.Drawing.Point(152, 63);
             this.LearningButton.Name = "LearningButton";
-            this.LearningButton.Size = new System.Drawing.Size(173, 59);
+            this.LearningButton.Size = new System.Drawing.Size(130, 48);
             this.LearningButton.TabIndex = 10;
             this.LearningButton.TabStop = false;
             this.LearningButton.Text = "Учить";
             this.LearningButton.UseSelectable = true;
+            this.LearningButton.Visible = false;
             this.LearningButton.Click += new System.EventHandler(this.LearningButton_Click);
             // 
             // ButtonHelp
             // 
             this.ButtonHelp.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ButtonHelp.Location = new System.Drawing.Point(23, 481);
-            this.ButtonHelp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ButtonHelp.Location = new System.Drawing.Point(17, 391);
+            this.ButtonHelp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ButtonHelp.Name = "ButtonHelp";
-            this.ButtonHelp.Size = new System.Drawing.Size(147, 23);
+            this.ButtonHelp.Size = new System.Drawing.Size(110, 19);
             this.ButtonHelp.TabIndex = 11;
             this.ButtonHelp.Text = "О программе";
             this.ButtonHelp.UseSelectable = true;
@@ -90,10 +90,10 @@ namespace SmartLearn
             // 
             this.MyDecks.FontSize = MetroFramework.MetroButtonSize.Medium;
             this.MyDecks.FontWeight = MetroFramework.MetroButtonWeight.Regular;
-            this.MyDecks.Location = new System.Drawing.Point(203, 289);
-            this.MyDecks.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MyDecks.Location = new System.Drawing.Point(152, 189);
+            this.MyDecks.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MyDecks.Name = "MyDecks";
-            this.MyDecks.Size = new System.Drawing.Size(173, 59);
+            this.MyDecks.Size = new System.Drawing.Size(130, 48);
             this.MyDecks.TabIndex = 12;
             this.MyDecks.TabStop = false;
             this.MyDecks.Text = "Мои колоды";
@@ -102,21 +102,20 @@ namespace SmartLearn
             // 
             // SmartLearn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(573, 529);
+            this.ClientSize = new System.Drawing.Size(430, 430);
             this.Controls.Add(this.MyDecks);
             this.Controls.Add(this.ButtonHelp);
             this.Controls.Add(this.LearningButton);
             this.Controls.Add(this.CheckTheme);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(573, 529);
-            this.MinimumSize = new System.Drawing.Size(573, 529);
+            this.MaximumSize = new System.Drawing.Size(430, 430);
+            this.MinimumSize = new System.Drawing.Size(430, 430);
             this.Name = "SmartLearn";
-            this.Padding = new System.Windows.Forms.Padding(20, 74, 20, 20);
+            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
             this.Text = "SmartLearn";
             this.Load += new System.EventHandler(this.SmartLearn_Load);
             ((System.ComponentModel.ISupportInitialize)(this.MainStyle)).EndInit();
