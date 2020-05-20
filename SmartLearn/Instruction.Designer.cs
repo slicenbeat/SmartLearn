7ede5e29115afa2803963cf5cc5e7622645e2b96
hitaboom <hitaboom@gmail.com>
2020-05-20 21:07:04 +0400

    Для инструкций

--- SmartLearn/Instruction.Designer.cs
+++ SmartLearn/Instruction.Designer.cs
@@ -31,41 +31,43 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Instruction));
             this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
             this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLink1 = new MetroFramework.Controls.MetroLink();
             this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
             this.metroTabPage3 = new MetroFramework.Controls.MetroTabPage();
             this.metroTabPage4 = new MetroFramework.Controls.MetroTabPage();
-            this.metroLink1 = new MetroFramework.Controls.MetroLink();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.metroTabControl1.SuspendLayout();
             this.metroTabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // metroTabControl1
             // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage4);
             this.metroTabControl1.Controls.Add(this.metroTabPage1);
             this.metroTabControl1.Controls.Add(this.metroTabPage2);
             this.metroTabControl1.Controls.Add(this.metroTabPage3);
-            this.metroTabControl1.Controls.Add(this.metroTabPage4);
             this.metroTabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.metroTabControl1.Location = new System.Drawing.Point(15, 49);
-            this.metroTabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroTabControl1.Location = new System.Drawing.Point(15, 60);
+            this.metroTabControl1.Margin = new System.Windows.Forms.Padding(2);
             this.metroTabControl1.Name = "metroTabControl1";
-            this.metroTabControl1.SelectedIndex = 3;
-            this.metroTabControl1.Size = new System.Drawing.Size(570, 301);
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(570, 290);
             this.metroTabControl1.TabIndex = 0;
             this.metroTabControl1.UseSelectable = true;
             // 
             // metroTabPage1
             // 
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
             this.metroTabPage1.Controls.Add(this.metroLink1);
             this.metroTabPage1.Cursor = System.Windows.Forms.Cursors.Default;
             this.metroTabPage1.HorizontalScrollbarBarColor = true;
             this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
             this.metroTabPage1.HorizontalScrollbarSize = 8;
             this.metroTabPage1.Location = new System.Drawing.Point(4, 38);
-            this.metroTabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroTabPage1.Margin = new System.Windows.Forms.Padding(2);
             this.metroTabPage1.Name = "metroTabPage1";
-            this.metroTabPage1.Size = new System.Drawing.Size(562, 259);
+            this.metroTabPage1.Size = new System.Drawing.Size(562, 248);
             this.metroTabPage1.TabIndex = 0;
             this.metroTabPage1.Text = "Открой список колод";
             this.metroTabPage1.VerticalScrollbarBarColor = true;
@@ -73,13 +75,22 @@
             this.metroTabPage1.VerticalScrollbarSize = 8;
             this.metroTabPage1.Click += new System.EventHandler(this.metroTabPage1_Click);
             // 
+            // metroLink1
+            // 
+            this.metroLink1.Location = new System.Drawing.Point(93, 45);
+            this.metroLink1.Name = "metroLink1";
+            this.metroLink1.Size = new System.Drawing.Size(75, 23);
+            this.metroLink1.TabIndex = 2;
+            this.metroLink1.Text = "metroLink1";
+            this.metroLink1.UseSelectable = true;
+            // 
             // metroTabPage2
             // 
             this.metroTabPage2.HorizontalScrollbarBarColor = true;
             this.metroTabPage2.HorizontalScrollbarHighlightOnWheel = false;
             this.metroTabPage2.HorizontalScrollbarSize = 8;
             this.metroTabPage2.Location = new System.Drawing.Point(4, 38);
-            this.metroTabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroTabPage2.Margin = new System.Windows.Forms.Padding(2);
             this.metroTabPage2.Name = "metroTabPage2";
             this.metroTabPage2.Size = new System.Drawing.Size(562, 259);
             this.metroTabPage2.TabIndex = 1;
@@ -94,7 +105,7 @@
             this.metroTabPage3.HorizontalScrollbarHighlightOnWheel = false;
             this.metroTabPage3.HorizontalScrollbarSize = 8;
             this.metroTabPage3.Location = new System.Drawing.Point(4, 38);
-            this.metroTabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroTabPage3.Margin = new System.Windows.Forms.Padding(2);
             this.metroTabPage3.Name = "metroTabPage3";
             this.metroTabPage3.Size = new System.Drawing.Size(562, 259);
             this.metroTabPage3.TabIndex = 2;
@@ -109,9 +120,9 @@
             this.metroTabPage4.HorizontalScrollbarHighlightOnWheel = false;
             this.metroTabPage4.HorizontalScrollbarSize = 8;
             this.metroTabPage4.Location = new System.Drawing.Point(4, 38);
-            this.metroTabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroTabPage4.Margin = new System.Windows.Forms.Padding(2);
             this.metroTabPage4.Name = "metroTabPage4";
-            this.metroTabPage4.Size = new System.Drawing.Size(562, 259);
+            this.metroTabPage4.Size = new System.Drawing.Size(562, 248);
             this.metroTabPage4.TabIndex = 3;
             this.metroTabPage4.Text = "Наслаждайся изучением!";
             this.metroTabPage4.VerticalScrollbarBarColor = true;
@@ -119,14 +130,14 @@
             this.metroTabPage4.VerticalScrollbarSize = 8;
             this.metroTabPage4.Click += new System.EventHandler(this.metroTabPage4_Click);
             // 
-            // metroLink1
+            // metroLabel1
             // 
-            this.metroLink1.Location = new System.Drawing.Point(93, 45);
-            this.metroLink1.Name = "metroLink1";
-            this.metroLink1.Size = new System.Drawing.Size(75, 23);
-            this.metroLink1.TabIndex = 2;
-            this.metroLink1.Text = "metroLink1";
-            this.metroLink1.UseSelectable = true;
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(297, 61);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(104, 133);
+            this.metroLabel1.TabIndex = 3;
+            this.metroLabel1.Text = "metroLabel1ыва\r\n\r\nыва\r\nыав\r\nыва\r\n\r\nыв";
             // 
             // Instruction
             // 
@@ -135,13 +146,14 @@
             this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.metroTabControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Instruction";
-            this.Padding = new System.Windows.Forms.Padding(15, 49, 15, 16);
+            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
             this.Text = "Instruction";
             this.Load += new System.EventHandler(this.Instruction_Load);
             this.metroTabControl1.ResumeLayout(false);
             this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -153,5 +165,6 @@
         private MetroFramework.Controls.MetroTabPage metroTabPage4;
         private MetroFramework.Controls.MetroTabControl metroTabControl1;
         private MetroFramework.Controls.MetroLink metroLink1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
     }
 }
